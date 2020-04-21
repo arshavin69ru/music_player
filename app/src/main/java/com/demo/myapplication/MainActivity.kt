d73e4b339f0f14c72e2320e431e5309b07c20965
vladimir <arshavin69ru@gmail.com>
2020-04-21 22:12:09 +0530

    First commit

---
+++ app/src/main/java/com/demo/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.demo.myapplication
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(com.demo.myapplication.R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.help -> {
+                Toast.makeText(this, "You need help", Toast.LENGTH_LONG).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
+
+
+
+
+
+
